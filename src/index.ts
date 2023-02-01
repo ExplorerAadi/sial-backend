aec63412f6f1503d2f3db954caf9f718c4797525
Aditya Raj <rajaditya27145@gmail.com>
2023-02-01 15:06:34 +0530

    test deployment

--- src/index.ts
+++ src/index.ts
@@ -25,6 +25,7 @@ const router = express.Router();
 const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
+app.use("/", () => console.info("⚡️[server]: Server is running"));
 app.use("/search", router.get("/", handler));
 
 app.listen(port, () => {
