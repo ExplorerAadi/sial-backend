1e94822f66ad2182ec1956ca8c6afaa1152c8a83
Aditya Raj <rajaditya27145@gmail.com>
2023-02-10 01:57:25 +0530

    handle errors gracefully

--- src/index.ts
+++ src/index.ts
@@ -11,12 +11,23 @@ const handler = async (req: Request, res: Response, next: NextFunction) => {
   });
   const openai = new OpenAIApi(configuration);
   const { ocassion, preferences, recipientRelationShip, budget } = req.query;
-  const response = await openai.createCompletion({
+  let response = await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: `Generate thoughtful gift recommendations for ${recipientRelationShip} on ${ocassion} whether materialistic (e.g. a handbag) or an experience (e.g. a spa session from UC). Their preferences, hobbies and interests are - ${preferences}. The budget is under ${budget} INR. Consider all this and recommend options from platforms that are easily available in India. Be as detailed about the suggestions and platforms as you can with links where necessary and limit them to 5 most accurate ones. Don't mention price explicitly in responses.`,
+    prompt: `Generate thoughtful gift recommendations for ${recipientRelationShip} on ${ocassion}. Their preferences, hobbies and interests are - ${preferences}. The budget is under ${budget} INR. Consider all this and show a variety of recommendations whether materialistic (e.g. a handbag) or an experience (e.g. a spa session from UC) from platforms that are easily available in India. Be as detailed about the suggestions and platforms as you can with links where necessary and limit them to 5 most accurate ones. Don't mention price explicitly in responses.`,
     max_tokens: 290,
     temperature: 0.7,
   });
+  if (response.status === 503) {
+    response = await openai.createCompletion({
+      model: "text-davinci-002",
+      prompt: `Generate thoughtful gift recommendations for ${recipientRelationShip} on ${ocassion}. Their preferences, hobbies and interests are - ${preferences}. The budget is under ${budget} INR. Consider all this and show a variety of recommendations whether materialistic (e.g. a handbag) or an experience (e.g. a spa session from UC) from platforms that are easily available in India. Be as detailed about the suggestions and platforms as you can with links where necessary and limit them to 5 most accurate ones. Don't mention price explicitly in responses.`,
+      max_tokens: 290,
+      temperature: 0.7,
+    });
+  }
+  if (response.status !== 200) {
+    res.status(500).json({ error: "Openai Server Error" });
+  }
   res.send(response.data);
 };
 
